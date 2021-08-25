89dbece0a1a784faa2053661ed3648445de1c233
suhas <avsuhas@gmail.com>
2021-08-25 18:52:29 -0400

    base commit

---
+++ codesamples/src/main/java/com/codesamples/CodesamplesApplication.java
@@ -0,0 +1,13 @@
+package com.codesamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesamplesApplication.class, args);
+	}
+
+}
